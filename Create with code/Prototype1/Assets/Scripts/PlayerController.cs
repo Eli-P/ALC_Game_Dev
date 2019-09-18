80ed84995255cf2b51f8d47232e9021fc345eb06
Elijah Palmer <eli.p561@stu.nebo.edu>
2019-09-18 10:29:26 -0600

    added vehicle movment

--- Create with code/Prototype1/Assets/Scripts/PlayerController.cs
+++ Create with code/Prototype1/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20f;
+    public float turnSpeed = 5;
+    public float horizontalInput;
+    public float forwardInput;
+    public float maxSpeed;
+    public float momentum;
+    float momentop = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -13,9 +19,44 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private float Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // We'll move the vehical forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        // smoother movement
+        if (System.Math.Abs(forwardInput) < 0 && momentum > 0)
+        {
+            momentum /= momentop;
+        }
+        else
+        {
+            if (momentum > maxSpeed)
+            {
+                momentum = maxSpeed;
+            }
+            else
+            {
+                if (System.Math.Abs(forwardInput - 1f) < 0)
+                {
+                    momentum += momentop;
+                }
+                else
+                {
+                    if (System.Math.Abs(forwardInput - -1) < 0)
+                    {
+                        momentum += momentop;
+                    }
+                }
+
+            }
+        }
+        
+        
+
     }
 }
