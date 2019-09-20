7069504fe3b4e91b5bcbcf88c38bf2f907c9294f
Elijah Palmer <eli.p561@stu.nebo.edu>
2019-09-20 10:31:15 -0600

    Challenge starteed

--- Create with code/Prototype1/Assets/Scripts/TestVarSpeed.cs
+++ Create with code/Prototype1/Assets/Scripts/TestVarSpeed.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class TestVarSpeed : MonoBehaviour
 {
     public float speed = 20f;
+    public float turnSpeed = 5;
+    public float horizontalInput;
+    public float forwardInput;
+    public float maxSpeed;
+    public float momentum;
+    private readonly float momentop = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +21,40 @@ public class TestVarSpeed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // We'll move the vehical forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * momentum);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (forwardInput < 0 && momentum > 0)
+        {
+            momentum /= momentop *2;
+        }
+        else
+        {
+            if (momentum > maxSpeed)
+            {
+                momentum = maxSpeed;
+            }
+            else
+            {
+                if (forwardInput > 0)
+                {
+                    momentum += momentop;
+                }
+                else
+                {
+                    if (forwardInput < 0)
+                    {
+                        momentum -= momentop;
+                    }
+                }
+
+            }
+        }
+
     }
 }
