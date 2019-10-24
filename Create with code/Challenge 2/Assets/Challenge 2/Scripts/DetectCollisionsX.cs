17df2e50c0013ac30085b3f92cc722fdc32d2797
Elijah Palmer <eli.p561@stu.nebo.edu>
2019-10-24 10:28:40 -0600

    Challenge 2 started

---
+++ Create with code/Challenge 2/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    public GameObject spawner;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Game Over!");
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+        Destroy(spawner);
+    }
+
+}
---
+++ Create with code/Challenge 2/Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+    public float speed;
+    private float leftLimit = -3000;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
