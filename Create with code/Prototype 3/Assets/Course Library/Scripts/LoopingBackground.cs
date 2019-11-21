dcf209b9352c5a8ce887eaf9e9febad9b811636d
eli.p <eli.p561@stu.nebo.edu>
2019-11-21 11:58:38 -0700

    Eureka! its working

--- Create with code/Prototype 3/Assets/Course Library/Scripts/LoopingBackground.cs
+++ Create with code/Prototype 3/Assets/Course Library/Scripts/LoopingBackground.cs
@@ -6,19 +6,23 @@ public class LoopingBackground : MonoBehaviour
 {
     private Vector3 startpos;
     private float repeatWidth;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         startpos = transform.position;
         repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startpos.x - repeatWidth)
+        if (transform.position.x < startpos.x - repeatWidth && playerControllerScript.GameOver == false)
         {
             transform.position = startpos;
         }
+
     }
 }
--- Create with code/Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
+++ Create with code/Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -6,21 +6,45 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody playerRb;
     public bool isOnGround = true;
+    public bool GameOver;
+    public float gravity;
+    public float jump;
+    private Animator player;
+    public ParticleSystem boom;
+    public ParticleSystem dirt;
+
     public void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        player = GetComponent<Animator>();
+        Physics.gravity *= gravity;
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !GameOver)
         {
-            playerRb.AddForce(Vector3.up * 1000);
+            playerRb.AddForce(Vector3.up * jump, ForceMode.Impulse);
             isOnGround = false;
+            player.SetTrigger("Jump_trig");
+            dirt.Stop();
         }
     }
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirt.Play();
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            player.SetBool("Death_b", true);
+            player.SetInteger("DeathType_int", 1);
+            GameOver = true;
+            dirt.Stop();
+            boom.Play();
+        }
+
     }
 }
