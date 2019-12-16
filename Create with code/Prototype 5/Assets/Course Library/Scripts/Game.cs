8795e14182f12c6e3e152473c5348b6d6b17b012
eli.p <eli.p561@stu.nebo.edu>
2019-12-16 10:07:59 -0700

    Started prototype 5

---
+++ Create with code/Prototype 5/Assets/Course Library/Scripts/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private float spawnRate = 1.0f;
+    public List<GameObject> targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator spawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
